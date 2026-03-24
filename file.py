8d88403c93db21cbfb30dc059079413a3f1afaec
luis kingsley <lkingsley00@gmail.com>
2026-03-24 09:24:31 -0400

    implement keyed file validation

--- file.py
+++ file.py
@@ -4,13 +4,9 @@ from keyed_file import KeyedFile
 
 KEYED_BLOCK_SIZE = 512
 FILE_SIZE = 0
-# Random file: fixed length records delimited by newlines
-# Sequential: variable length records delimited by newlines
-# Direct: Data one after another without any delimiter
-
-# TODO:1. check if file is keyed
-# TODO:2. check if file has only one line
-
+# Random file: Fixed length records delimited by newlines
+# Sequential: Variable length records delimited by newlines
+# Direct: Records one after another without any delimiter or new lines
 
 # Check if file is provided as argument
 if len(sys.argv) != 2:
@@ -23,31 +19,37 @@ if not os.path.isfile(sys.argv[1]):
 # Check if file is empty
 FILE_SIZE = os.path.getsize(sys.argv[1])
 if FILE_SIZE == 0:
-    quit('Empty file')
+    print('Empty file')
+    quit(0)
 
 def is_keyed():
     kfile = KeyedFile()
+    # Check if file size is multiple of 512
     if FILE_SIZE % KEYED_BLOCK_SIZE:
-        print('not multiple')
-    with open(sys.argv[1], 'rb') as f:
-        first_block = f.read(512)
-        kfile.og_file_name = first_block[12:30]
+        return
 
-        year = first_block[30] | first_block[31] << 8
-        month = first_block[32]
-        day = first_block[33]
+    with open(sys.argv[1], 'rb') as f:
+        control_block = f.read(KEYED_BLOCK_SIZE)
+        kfile.og_file_name = control_block[12:30]
+
+        year = control_block[30] | control_block[31] << 8
+        month = control_block[32]
+        day = control_block[33]
         kfile.creation_date = '{}-{}-{}'.format(year, month, day)
         
-        kfile.blocks_qty = first_block[42] | \
-                        (first_block[43] << 8) | \
-                        (first_block[44] << 16) | \
-                        (first_block[45] << 24)
-        kfile.key_length = first_block[54] | (first_block[55] << 8)
-        kfile.record_size = first_block[46] | (first_block[47] << 8)
-        kfile.print()
-# TODO: finish keyed file validation
+        kfile.blocks_qty = control_block[42] | \
+                        (control_block[43] << 8) | \
+                        (control_block[44] << 16) | \
+                        (control_block[45] << 24)
+        kfile.key_length = control_block[54] | (control_block[55] << 8)
+        kfile.record_size = control_block[46] | (control_block[47] << 8)
+        
+        if kfile.blocks_qty * KEYED_BLOCK_SIZE != FILE_SIZE:
+            return
+        print('Keyed file')
+        quit(0)
+
 is_keyed()
-quit(0)
 
 with open(sys.argv[1], 'rb') as f:
     if len(f.readlines()) == 1:
@@ -56,17 +58,18 @@ with open(sys.argv[1], 'rb') as f:
 
 
 with open(sys.argv[1], 'rb') as f:
-    first_line_length = len(f.readline())
     is_random = False
+    first_line_length = len(f.readline())
+    
     for line in f:
         if first_line_length == len(line):
             is_random = True
         else:
             is_random = False
             break
+
     if is_random:
         print('Random file')
-        quit()
+        quit(0)
     else:
         print('Sequential file')
-        quit(0)
