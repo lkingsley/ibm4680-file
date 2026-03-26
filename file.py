7e2b4ecffbaef2dfc94ea1960c007684ddc7030b
lkingsley <lkingsley00@gmail.com>
2026-03-25 23:29:40 -0400

    code refactoring

--- file.py
+++ file.py
@@ -8,19 +8,18 @@ from util import is_direct, is_empty, is_keyed, is_random
 # Random file: Fixed length records delimited by CR/LF
 # Sequential file: Variable length records delimited by CR/LF
 
-# Check if file is provided as argument
-if len(sys.argv) != 2:
-    quit('Usage: file.py <filename>')
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        quit('Usage: file.py <filename>')
 
-file_name = sys.argv[1]
+    file_name = sys.argv[1]
+    
+    if not os.path.isfile(file_name):
+        quit('File does not exist')
 
-# Check if file exists
-if not os.path.isfile(file_name):
-    quit('File does not exist')
-
-is_empty(file_name)
-is_keyed(file_name)
-is_direct(file_name)
-is_random(file_name)
-
-print('Sequential file')
+    is_empty(file_name)
+    is_keyed(file_name)
+    is_direct(file_name)
+    is_random(file_name)
+    
+    print('Sequential file')
