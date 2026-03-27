4467efad146f7ff3bc40d1ff46994507bbacf558
luis kingsley <lkingsley00@gmail.com>
2026-03-27 12:50:19 -0400

    code refactoring

--- util.py
+++ util.py
@@ -4,7 +4,11 @@ from keyed_file import KeyedFile, BLOCK_SIZE
 
 
 def hex_to_int(hex_string):
-    return int(hex_string.encode('hex'), 16)
+    # Works on python 2.7.18
+    if hasattr(hex_string, 'encode'):
+        return int(hex_string.encode('hex'), 16)
+    else:
+        return hex_string
 
 
 def is_direct(file_name):
@@ -33,17 +37,21 @@ def is_keyed(file_name):
         control_block = f.read(BLOCK_SIZE)
         kfile.og_file_name = control_block[12:30].decode(
             encoding='utf-8', errors='ignore').rstrip('\x00')
-                                    
-        year = hex_to_int(control_block[30]) | (hex_to_int(control_block[31]) << 8)
+
+        year = hex_to_int(control_block[30]) | \
+            (hex_to_int(control_block[31]) << 8)
         month = hex_to_int(control_block[32])
         day = hex_to_int(control_block[33])
         kfile.creation_date = '{}/{}/{}'.format(year, month, day)
-        
-        kfile.blocks_qty = hex_to_int(control_block[42]) | (hex_to_int(control_block[43]) << 8) | \
+
+        kfile.blocks_qty = hex_to_int(control_block[42]) | \
+            (hex_to_int(control_block[43]) << 8) | \
             (hex_to_int(control_block[44]) << 16) | \
             (hex_to_int(control_block[45]) << 24)
-        kfile.key_length = hex_to_int(control_block[54]) | (hex_to_int(control_block[55]) << 8)
-        kfile.record_size = hex_to_int(control_block[46]) | (hex_to_int(control_block[47]) << 8)
+        kfile.key_length = hex_to_int(control_block[54]) | \
+            (hex_to_int(control_block[55]) << 8)
+        kfile.record_size = hex_to_int(control_block[46]) | \
+            (hex_to_int(control_block[47]) << 8)
 
         if kfile.blocks_qty * BLOCK_SIZE != file_size:
             return
